34067b0253891623bd5561ab0ece7881de1b78ee
vishal chaudhary <vishalchaudhary1975@gmail.com>
2021-12-09 23:37:43 +0530

    first commit

---
+++ javascript/registeration.js
@@ -0,0 +1,74 @@
+let fullname,username,email,phonenumber,password,confirmpassword;
+
+function getdetails(){
+
+ fullname=document.getElementById('name').value;
+ username=document.getElementById('username').value;
+ email=document.getElementById('email').value;
+ phonenumber=document.getElementById('phonenumber').value;
+ password=document.getElementById('password').value
+ confirmpassword=document.getElementById('confirmpassword').value
+
+}
+let formsubmit=document.getElementById( 'formsubmit');
+
+ formsubmit.addEventListener('click',(e)=>{
+
+e.preventDefault();
+
+getdetails()
+const para = document.getElementById('error');
+para.textContent=""
+if(password!=confirmpassword){
+    
+    const node = document.createTextNode("please check your password");
+    para.appendChild(node);
+    para.style.color='red'
+    
+}
+else{
+para.textContent=""
+postdata();
+
+
+
+
+}
+
+})
+
+function postdata(){
+  let data=JSON.stringify({"fullname":fullname,"username":username,"email":email,
+           "phonenumber":phonenumber,"password":password});
+          addUser(data);
+          //  fetch("http://localhost:8080/user/add",{
+
+          //  method:'POST',
+          //  headers:{
+          //   'Content-Type': 'application/json'
+          //  },
+          //  body:data
+          //  }).then((response)=>response.text())
+          //    .then((data)=>{
+          //      console.log(data);
+          //    })
+
+}
+
+async function addUser(data){
+
+let response=await fetch("http://localhost:8080/user/add",{
+
+  method:'POST',
+  headers:{
+   'Content-Type': 'application/json'
+  },
+  body:data
+  })
+
+  if(response.ok){
+  let responsebody=await response.text()
+  console.log(responsebody)
+  window.location.replace("http://127.0.0.1:5500/Html/login.html");
+  }
+}
