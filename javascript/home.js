7582cc0e4acefdf1da6b76a1d8a0ba07afe71680
vishal chaudhary <vishalchaudhary1975@gmail.com>
2021-12-22 01:21:08 +0530

    message box added

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,9 @@
 var box  = document.getElementById('box');
+var message_boxe = document.getElementById('message-box')
+var container = document.getElementById('container')
+//var message_boxe = document.getElementsByClassName("message-box")
 var down = false;
-
+var open=false;// varriable for message box
 
 function toggleNotifi(){
 	if (down) {
@@ -12,4 +15,29 @@ function toggleNotifi(){
 		box.style.opacity = 1;
 		down = true;
 	}
+}
+
+function message_box(){
+if(open){
+
+ message_boxe.style.visibility='hidden'
+ 
+ open=false;
+}
+else{
+	container.style.color='grey'
+ container.style.opacity=0.3
+	message_boxe.style.visibility='visible'
+	open=true;
+}
+}
+
+function message_box_close(){
+	if(open){
+		container.style.opacity=1
+		message_boxe.style.visibility='hidden'
+		
+		container.style.visibility='bg-primary'
+		open=false;
+	   }
 }
